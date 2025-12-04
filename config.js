b961a06befddc37153e00dce783ddb350c038bc5
Santoshi Atmakuru <50026883+satmakuru222@users.noreply.github.com>
2025-12-04 12:16:24 -0500

    Remove console log statements from config.js
    Removed console log statements for cleaner code.

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-console.log(import.meta);
-console.log(import.meta.env);
-
 export const supabaseConfig = {
     url: import.meta.env.VITE_SUPABASE_URL,
     anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY
