9d9511616d609b8d74146022115d49dd32184fd2
Santoshi Atmakuru <50026883+satmakuru222@users.noreply.github.com>
2025-12-04 12:13:07 -0500

    Log import.meta and import.meta.env in config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+console.log(import.meta);
+console.log(import.meta.env);
+
 export const supabaseConfig = {
     url: import.meta.env.VITE_SUPABASE_URL,
     anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY
